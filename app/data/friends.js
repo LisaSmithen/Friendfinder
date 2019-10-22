c05e8104e5c6255761967b7c1eedcaf6d77fd2b5
Lisa Smithen <smithenlisa@gmail.com>
2019-10-22 18:52:32 -0500

    Friends.js was updated with coding to reflect possible friend choices. Still need to get the code to actually display an option and then front styling

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friendsArray = [{
+	"name":"Carebear Carealot",
+	"photo":"https://vignette.wikia.nocookie.net/carebears/images/7/70/LAL.png/revision/latest?cb=20161102132440",
+	"scores":[3, 4, 5, 5, 5, 5, 5, 3, 3, 4]
+}, {
+	"name":"Moody Maddie",
+	"photo":"http://modelsintro.com/gallery/loadimage.php?token=maggie13moodie%2BBsWnsOAggqp",
+	"scores":[1, 2, 1, 1, 2, 1, 1, 5, 2, 5]	
+}, {
+	"name":"Lora Croft",
+	"photo":"https://m.media-amazon.com/images/M/MV5BMzk4NWM5YjgtMGM5Ny00NTEwLTljMjItOGU2ZTcwMTJjMjhiXkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_CR0,45,480,270_AL_UX477_CR0,0,477,268_AL_.jpg",
+	"scores":[5, 2, 3, 3, 4, 5, 4, 2, 3, 1]
+}, {
+	"name":"Martha",
+	"photo":"https://cdn.vox-cdn.com/thumbor/M8ez9xI_fTMD5RBclLwoou0vICM=/0x0:987x659/1200x800/filters:focal(416x252:572x408)/cdn.vox-cdn.com/uploads/chorus_image/image/54599973/Screen_Shot_2017_05_02_at_4.10.22_PM.0.png",
+	"scores":[2, 5, 3, 3, 3, 3, 3, 4, 1, 5]
+}, {
+	"name":"Optimus Prime",
+	"photo":"https://tfwiki.net/mediawiki/images2/thumb/3/37/Optimusg1.jpg/350px-Optimusg1.jpg",
+	"scores":[5, 1, 5, 5, 3, 4, 4, 2, 4, 1]
+}, {
+	"name":"Gordan",
+	"photo":"https://static.tvtropes.org/pmwiki/pub/images/0a3e6875e0603de1adc901657a7077a6.jpg",
+	"scores":[5, 5, 2, 2, 5, 2, 2, 5, 1, 5]
+}, {
+	"name":"Tupac",
+	"photo":"",
+	"scores":[3, 1, 3, 4, 4, 5, 2, 5, 1, 1]
+}, {
+	"name":"Jennelle",
+	"photo":"https://www.outsideonline.com/sites/default/files/styles/img_600x600/public/2019/09/06/jennelle-eliana-youtube_s.jpg?itok=pp8bymCg",
+	"scores":[5, 3, 4, 5, 5, 3, 4, 4, 5, 1]
+}, {
+	"name":"Rhianna",
+	"photo":"https://i.pinimg.com/originals/2d/1f/e6/2d1fe6cab7df9c0c023b647a99d0de66.jpg",
+	"scores":[5, 5, 5, 3, 4, 5, 4, 4, 5, 1]
+}];
+
+module.exports = friendsArray;
